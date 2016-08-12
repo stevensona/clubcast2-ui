9e9c1ac26a3032b67201097970f33ec51233d3bd
stevensona <adamstevenson121@gmail.com>
2016-08-12 14:59:06 -0400

    use component for episode card

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+var Episode = Vue.extend({
+  props: ['episode', 'play', 'thumbnail'],
+  template: `
+    <div class="column is-one-third">
+      <div class="card">
+        <div class="card-image" @click="play(episode)">
+          <figure class="image is-square"><img v-bind:src="episode.image" alt=""></figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-32x32"><img v-bind:src="thumbnail" alt=""></figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-5">{{episode.title}}</p>
+              <p class="subtitle is-6">{{episode.artist}}</p>
+            </div>
+          </div>
+          <div class="content">
+            {{episode.duration}}
+            <small>{{episode.date}}</small>
+          </div>
+        </div>
+      </div>
+    </div>`
+});
+
+Vue.component('episode', Episode);
+
 new Vue({
   el: '#app',
   data: {
